e4d2ef289de0cf1a37d819fb01f0d55971168b7d
juanmi <juanmidelatorre@hotmail.es>
2017-08-20 19:16:20 +0200

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,6 +6,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean=true;
 
   constructor( private http:Http ) {
@@ -13,24 +14,52 @@ export class ProductosService {
   }
 
   public cargar_producto( cod:string ){
-    console.log(cod);
+    //console.log(cod);
     return this.http.get(`https://junglaanimal-57cc9.firebaseio.com/productos/${ cod }.json`);
 
+  }
+
+  public buscar_producto( termino:string ){
+    //console.log("Buscando producto");
+    if ( this.productos.length ===0 ) {
+        this.cargar_productos().then(()=>{
+          this.filtrar_productos( termino );
+        });
+    } else {
+      this.filtrar_productos( termino );
+    }
+
+
+  }
+
+  private filtrar_productos(termino:string){
+    this.productos_filtrado=[];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+      if ( prod.categoria.toLowerCase().indexOf( termino ) >=0 || prod.titulo.toLowerCase().indexOf( termino ) >=0){
+        this.productos_filtrado.push( prod );
+      }
+      //console.log(prod);
+    })
   }
 
   public cargar_productos(){
     this.cargando=true;
 
-    if ( this.productos.length === 0 ){
+    let promesa = new Promise( (resolve, reject)=>{
       this.http.get('https://junglaanimal-57cc9.firebaseio.com/productos_idx.json')
         .subscribe( res => {
           //console.log( res.json() );
           this.productos=res.json();
           this.cargando=false;
-        })
+          resolve();
+        });
 
+    });
+    return promesa;
 
-    }
 
   }
 }
