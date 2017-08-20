552024119fd55df263378f07025c90bded2d3fa4
juanmi <juanmidelatorre@hotmail.es>
2017-08-20 17:08:15 +0200

    Agregamos firebase al portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,6 +19,7 @@ export class ProductosService {
       this.http.get('https://junglaanimal-57cc9.firebaseio.com/productos_idx.json')
         .subscribe( res => {
           console.log( res.json() );
+          this.productos=res.json();
           this.cargando=false;
         })
 
